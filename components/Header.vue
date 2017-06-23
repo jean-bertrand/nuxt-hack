ae52cdc14e141958586c34134daeac352aa27b91
mattjared <mattjared9@gmail.com>
2017-06-22 23:44:20 -0500

    first pass at this

---
+++ components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Hey hey!!</h2>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <site-header/>
+  </div>
+</template>
+
+<script>
+  import SiteHeader from '~components/Header.vue'
+
+  export default {
+    components: {
+      SiteHeader
+    }
+  }
+</script>
